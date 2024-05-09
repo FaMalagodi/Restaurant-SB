5739955bc6e28c1b61644e59617dd54acc5452b0
FaMalagodi <141671142+FaMalagodi@users.noreply.github.com>
2024-05-09 15:50:55 -0300

    Back-end

---
+++ BACK-END/dbconfig.js
@@ -0,0 +1,12 @@
+const config = {
+  user: 'BD23331',
+  password: 'BD23331',
+  server: 'regulus.cotuca.unicamp.br',
+  database: 'BD23331',
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+module.exports = config;
